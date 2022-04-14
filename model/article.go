3ed534009b7ad588c81318b984382f22472ae382
b-waynebai <yue11037417@gmail.com>
2022-04-14 17:17:57 +0800

    添加外键关联

--- model/article.go
+++ model/article.go
@@ -3,11 +3,11 @@ package model
 import "gorm.io/gorm"
 
 type Article struct {
+	Category Category `gorm:"foreignkey:Cid"`
 	gorm.Model
-	Name string `gorm:"column:name;type:varchar(20);not null" json:"name"`
-	Title string `gorm:"column:title;type:varchar(20);not null" json:"title"`
-	Cid int `gorm:"column:cid;type:int(20);not null" json:"cid"`
-	Desc string `gorm:"column:name;type:varchar(200);not null" json:"desc"`
+	Title   string `gorm:"column:title;type:varchar(20);not null" json:"title"`
+	Cid     int    `gorm:"column:cid;type:int(20);not null" json:"cid"`
+	Desc    string `gorm:"column:name;type:varchar(200);not null" json:"desc"`
 	Content string `gorm:"column:Content;type:longtext" json:"content"`
-	Img string `gorm:"column:img;type:varchar(20)" json:"img"`
+	Img     string `gorm:"column:img;type:varchar(20)" json:"img"`
 }
--- model/category.go
+++ model/category.go
@@ -1,8 +1,6 @@
 package model
 
-import "gorm.io/gorm"
-
-type  Category struct {
-	gorm.Model
+type Category struct {
+	Id   int    `gorm:"column:id;type:int(20);not null" json:"id"`
 	Name string `gorm:"column:name;type:varchar(20);not null" json:"name"`
 }
